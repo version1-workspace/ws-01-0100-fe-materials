568421291267909ec844893a323df0dcb32b0b70
Jiro <j.code.public@gmail.com>
2024-12-23 07:45:23 -0800

    [0700-nest-web-api] add swagger UI

---
+++ 0700-nest-web-api/src/app/api/v1/spec/swagger.yml/route.ts
@@ -0,0 +1,23 @@
+import fs from "fs";
+import path from "path";
+
+export function GET() {
+  const filePath = path.join(
+    process.cwd(),
+    "src/app/api/v1/spec/swagger.yml/swagger.yml",
+  );
+
+  // ファイルの内容を読み込む
+  try {
+    const file = fs.readFileSync(filePath, "utf8");
+
+    return new Response(file, {
+      status: 200,
+    });
+  } catch (e) {
+    console.error("Got error on endpoint", e);
+    return new Response(`Not Found`, {
+      status: 404,
+    });
+  }
+}
